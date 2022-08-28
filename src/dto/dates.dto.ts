80f6da2ccd1b182f4f8bc348034b8375ee06e413
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-28 16:00:45 +0200

    fixes

--- src/dto/dates.dto.ts
+++ src/dto/dates.dto.ts
@@ -1,7 +1,8 @@
-import { IsBoolean } from 'class-validator';
+import {IsBoolean, IsOptional} from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class DatesDto {
+  @IsOptional()
   @IsBoolean()
   @Transform(({ value }) => value === 'true')
   dates?: boolean;
