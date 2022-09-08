845e2aa32089d77a8b046c547738ec288e572d11
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-08 20:37:57 +0200

    get images

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param, ParseIntPipe} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { ImageService } from './image.service';
 
 @ApiTags('Images')
 @Controller('image')
-export class ImageController {}
+export class ImageController {
+  constructor(private readonly imageService: ImageService) {}
+
+  @Get(':width/:height')
+  getImage(
+    @Param('width', ParseIntPipe) width: number,
+    @Param('height', ParseIntPipe) height: number
+  ) {
+    return this.imageService.getImage({ width, height });
+  }
+}
--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ImageService {}
+export class ImageService {
+  async getImage({ width, height }: { width: number; height: number }) {
+    //
+  }
+}
